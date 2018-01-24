ca233dba7912e4662ba10b0420c06b8f6a45c357
Sadraque <drakesad@gmail.com>
2018-01-24 12:34:56 -0200

    connected list in alusnosdb

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -15,24 +15,27 @@ angular.module("meuModulo",['ngRoute'])
 
 })
 
-.factory('lista', function(){  
 
-	alunos = [
-	{nome: "Vader",email:"vader@mail.com",sexo:"Masculino",religiao:"Cristianismo",data:"18/01/2015"},
-	{nome: "Luke",email:"luke@mail.com",sexo:"Masculino",religiao:"Budismo",data:"23/12/2014"},
-	{nome: "Leia",email:"leia@mail.com",sexo:"Feminino",religiao:"Deboismo",data:"30/01/2017"},
-	{nome: "Han",email:"han@mail.com",sexo:"Masculino",religiao:"Cristianismo",data:"15/10/1991"},
-	{nome: "Yoda",email:"yoda@mail.com",sexo:"Masculino",religiao:"Cristianismo",data:"11/11/2000"}
-    ];
+
+
+
+.factory('lista', function($http){ 
+
+
+	var alunos;
+
+
     return {
         addAluno : function (aluno) {
             alunos.push(aluno);
         },
         getAlunos : function () {
+        	console.log(alunos);
+        	console.log("chamou");
             return alunos;
         },
         removeAluno : function(indice){
         	alunos.splice(indice,1);
         }
-    }    
+    }      
  });
